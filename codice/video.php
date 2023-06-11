c50f8d8608eb375b58858dbe01cd2b47dd5dd01c
lallabelloni <belloni.laura00@gmail.com>
2023-06-11 18:22:04 +0200

    video da finirre di correggere

--- codice/video.php
+++ codice/video.php
@@ -6,7 +6,6 @@
   <!-- <link rel="stylesheet" type="text/css" href="../css/root.css"> -->
   <!-- <link rel="stylesheet" type="text/css" href="../css/navbar.css"> -->
   <!-- <link rel="stylesheet" type="text/css" href="../css/footer.css"> -->
-  <!-- deve essere un solo css sia per comprato che NOcomprato -->
   <!-- <link rel="stylesheet" type="text/css" href=".../css/video.css"> -->
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
@@ -24,29 +23,36 @@ if(!isset($_SESSION['id_corso'])){
   echo "<p> Non ci sono video selezionati</p>";
   exit();
 }
-$id_corso = $_SESSION['id_corso'];
-$id_utente = $_SESSION['id_utente'];
-// echo "rteug ":$id_corso;
-require 'db.php';
-$conn = new mysqli($hostData, $userData, $paswData, $database);
-$sql = "SELECT * FROM Acquisti 
-        WHERE id_utente = ?
-        AND id_corso = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("si", $id_utente, $id_corso);
-$stmt->execute();
-$result = $stmt->get_result();
+if(!isset($_SESSION['id_utente'])){
+  $id_corso = $_SESSION['id_corso'];
+  echo "no sessione";
+  // include 'video-NOcomprato.php';
+} else {
+  echo "sessione";
+  $id_corso = $_SESSION['id_corso'];
+  $id_utente = $_SESSION['id_utente'];
 
-if ($result->num_rows <= 0){
-  echo "<p>Non hai comprato questo corso </p>";
-  include 'video-NOcomprato.php';
+  require 'db.php';
+  $conn = new mysqli($hostData, $userData, $paswData, $database);
+  $sql = "SELECT * FROM Acquisti 
+          WHERE id_utente = ?
+          AND id_corso = ?";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("si", $id_utente, $id_corso);
+  $stmt->execute();
+  $result = $stmt->get_result();
+
+  if ($result->num_rows <= 0){
+    echo "<p>Non hai comprato questo corso </p>";
+    // include 'video-NOcomprato.php';
+  }
+  else {
+    echo "<p>Hai comprato questo corso </p>";
+    // include 'video-comprato.php';
+  }
+  $stmt->close();
+  $conn->close();
 }
-else {
-  echo "<p>Hai comprato questo corso </p>";
-  include 'video-comprato.php';
-}
-$stmt->close();
-$conn->close();
 ?>
 
 <?php //include 'footer.php';?>
