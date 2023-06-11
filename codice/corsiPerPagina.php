4700b767f9e396b6de5745637ac40a88957810fa
lallabelloni <belloni.laura00@gmail.com>
2023-06-11 11:48:26 +0200

    css home finita

--- codice/corsiPerPagina.php
+++ codice/corsiPerPagina.php
@@ -42,24 +42,31 @@ if ($resultWithLimit->num_rows > 0) {
     $descrizioneBreve = implode(' ', $primeDieciParole);
     ?>
     <button class="vaiAlCorso" data-id-corso="<?php echo $id; ?>">
-    <img src='<?php echo $urlImg; ?>' alt='<?php echo $altImg; ?>' height="300">
-    <h2><?php echo $titolo; ?></h2>
-    <p><?php echo $autore; ?></p>
-    <p><?php echo $descrizioneBreve." ..."; ?></p>
-    <p> 
-    <?php //stelline
-    if ($valutazioneMedia !== NULL && $valutazioneMedia >= 0 && $valutazioneMedia < 6) {
-      $gialla = $valutazioneMedia;
-      $grigia = 5-$valutazioneMedia;
-      // togliere height="30"
-      for ($i = 0; $i < $gialla; $i++) { echo '<img src="../assets/icon/star/gialla.svg" alt="stella gialla" height="30">'; }
-      for ($i = 0; $i < $grigia; $i++) { echo '<img src="../assets/icon/star/grigia.svg" alt="stella grigia" height="30">'; }
-    }
-    if ($valutazioneMedia === NULL) {
-      for ($i = 0; $i < 5; $i++) { echo '<img src="../assets/icon/star/grigia.svg" alt="stella grigia" height="30">'; }
-    }
-    ?></p>
-    <p><?php echo "Valutata da " . $nUtentiValut . " utenti";?></p>
+      <img class="img-corso" src='<?php echo $urlImg; ?>' alt='<?php echo $altImg; ?>'>
+      <div class="info">
+        <div class="texts"> 
+          <h2><?php echo $titolo; ?></h2>
+          <p><?php echo $autore; ?></p>
+          <p><?php echo $descrizioneBreve." ..."; ?></p>
+        </div>
+        <div class="rating"> 
+          <div class="stars"> 
+            <?php //stelline
+            if ($valutazioneMedia !== NULL && $valutazioneMedia >= 0 && $valutazioneMedia < 6) {
+              $gialla = $valutazioneMedia;
+              $grigia = 5-$valutazioneMedia;
+              // togliere height="30"
+              for ($i = 0; $i < $gialla; $i++) { echo '<img src="../assets/icon/star/gialla.svg" alt="stella gialla" height="30">'; }
+              for ($i = 0; $i < $grigia; $i++) { echo '<img src="../assets/icon/star/grigia.svg" alt="stella grigia" height="30">'; }
+            }
+            if ($valutazioneMedia === NULL) {
+              for ($i = 0; $i < 5; $i++) { echo '<img src="../assets/icon/star/grigia.svg" alt="stella grigia" height="30">'; }
+            }
+            ?>
+          </div>
+        <p><?php echo "Valutata da " . $nUtentiValut . " utenti";?></p>
+        </div>
+      </div>
     </button>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="../typescript/click.js"></script>
--- codice/home.php
+++ codice/home.php
@@ -1,91 +1,146 @@
 <!DOCTYPE html>
 <html lang="it">
-<head>
-   <meta charset="UTF-8">
-   <link rel="stylesheet" type="text/css" href="../css/root.css">
-   <link rel="stylesheet" type="text/css" href="../css/navbar.css">
-   <link rel="stylesheet" type="text/css" href="../css/footer.css">
-   <link rel="stylesheet" type="text/css" href="../css/home.css">
-   <title>LAMA</title>
-   <meta name="keywords " content="LAMA">
-   <meta name="author " content="Belloni Laura, Contegno Matteo">
-</head>
-<body>
+   <head>
+      <meta charset="UTF-8">
+      <link rel="stylesheet" type="text/css" href="../css/root.css">
+      <link rel="stylesheet" type="text/css" href="../css/navbar.css">
+      <link rel="stylesheet" type="text/css" href="../css/footer.css">
+      <link rel="stylesheet" type="text/css" href="../css/home.css">
+      <title>LAMA</title>
+      <meta name="keywords " content="LAMA">
+      <meta name="author " content="Belloni Laura, Contegno Matteo">
+   </head>
+   <body>
 
-<?php require 'sessionStart.php';?>
-   <div class="nav">
-      <?php include 'navbar.php';?>
-   </div>
+      <?php require 'sessionStart.php';?>
 
-<div class="pag1">
-   <p><?php echo isset($_SESSION["nomeutente"]) ? "Benvenutx ".$_SESSION["nomeutente"] : "Impara con LAMA"; ?></p>
-   <p>I tuoi corsi ovunque!</p>
-   <p>Trova quello<br>fatto apposta per te</p>
-</div>
+      <div class="nav">
+         <?php //include 'navbar.php';?>
+      </div>
 
-<div class="pag2">
-<p>I <b>nostri</b> corsi</p>
-   <?php
-   if(isset($_COOKIE['primaAccedi'])){
-      header("Location: carrello.php");
-      exit();
-   }
-   if(isset($_COOKie['acquistaOra'])){ // fai un cookieeeeee!!!
-      header("Location: video.php");
-      exit();
-   }
-   
-   $corsiPerPagina = 3;
+      <div class="home">
+         
+         <div class="section-1">
+            <div class="section-1__left">
+               <p><?php echo isset($_SESSION["nomeutente"]) ? "Benvenutx ".$_SESSION["nomeutente"] : "Impara con <br>LAMA"; ?></p>
+               <p>I tuoi corsi ovunque!</p>
+               <!-- <img src="../assets/img/wave.png">  -->
+            </div>
+            <div class="section-1__right">
+               <p>Trova quello<br>fatto apposta per <span>TE</span></p>
+            </div>
+         </div>
 
-   require 'db.php';
-   $conn = new mysqli($hostData, $userData, $paswData, $database);
-   
-   $sql="SELECT * FROM Corsi";
-   $stmt = $conn->prepare($sql);
+         <div class="section-2">
+            <p>I nostri corsi</p>
 
-   $sqlCount="SELECT COUNT(*) AS total FROM Corsi";
-   $stmtCount = $conn->prepare($sqlCount);
+            <div class="section-2__corsi">
+               <?php
+                  if(isset($_COOKIE['primaAccedi'])){
+                     header("Location: carrello.php");
+                     exit();
+                  }
+                  if(isset($_COOKie['acquistaOra'])){ // fai un cookieeeeee!!!
+                     header("Location: video.php");
+                     exit();
+                  }
+                  
+                  $corsiPerPagina = 6;
 
-   $start = "";
-   $end = "";
-   require "corsiPerPagina.php";
-   $stmt->close();
-   $conn->close();
-   ?>
-   <a href="cerca.php"><button>Vai a tutti i corsi</button></a>
-</div>
+                  require 'db.php';
+                  $conn = new mysqli($hostData, $userData, $paswData, $database);
+                  
+                  $sql="SELECT * FROM Corsi";
+                  $stmt = $conn->prepare($sql);
 
-<div class="pag3">  sfondo immagine grafico
-   <p>LAMA ti fa scoprire il<br>mondo</p>
-</div>
+                  $sqlCount="SELECT COUNT(*) AS total FROM Corsi";
+                  $stmtCount = $conn->prepare($sqlCount);
 
-<div class="pag4">
-   <p>Il tuo spazio<br>per imparare</p>
-   <p>Che sia la tua scrivania, il caffè<br>sotto casa o il treno</p>
-   <p>trova il tuo spazio per IMPARARE!</p>
-   <img src="src/assets/img/" alt="Disegno di una persona alla scrivania al computer che mangia mentre una persona in pigiama si dirige alla propria scrivania"> 
-</div>
+                  $start = "";
+                  $end = "";
+                  require "corsiPerPagina.php";
+                  $stmt->close();
+                  $conn->close();
+               ?>
+            </div>
+            <a href="cerca.php">Vai a tutti i corsi</a>
+         </div>
 
-<div class="pag5">
-   <p>Ritrova le tue<br>passioni</p>
-   <p>Prenditi il tempo per te e per i tuoi<br>interessi,</p>
-   <p>ora è FACILE.</p>
-   <img src="src/assets/img/" alt="Disegno di una persona alla scrivania al computer di schiena con una camicia bianca e circondata da piante"> 
-</div> 
+         <div class="section-3">  
+            <p>LAMA ti fa scoprire il<br>mondo</p>
+            <img src="../assets/img/geometry.svg">
+         </div>
 
-<div class="pag6">  sfondo immagine bambina
-   <p>Per tutte<br>le età</p>
-   <p>Non esiste età GIUSTA<br>Per imparare!</p>
-   <img src="src/assets/img/" alt="Disegno di 5 persone di età diverse che si abbracciano, impoteticamente una famiglia con nonni e bambini"> 
-</div>
+         <div class="section-4">
+            <div class="section-4__left">
+               <p>
+                  Il tuo spazio
+                  <br>per imparare
+               </p>
+               <p>
+                  Che sia la tua scrivania, il caffè
+                  <br>sotto casa o il treno
+                  <br><br>trova il tuo spazio per IMPARARE!
+               </p>
+            </div>
+            <div class="section-4__right">
+               <img src="../assets/img/girl-computer-1.svg" alt="Disegno di una persona alla scrivania al computer che mangia mentre una persona in pigiama si dirige alla propria scrivania"> 
+            </div>  
+         </div>
 
-<div class="pag7">
-   <p>impara<br>edivertiti</p>
-   <p>Condividi le lezioni con le<br>persone che ami</p>
-   <p>Una nuova opportunità per<br>imparare e divertirsi INSIEME.</p>
-   <img src="src/assets/img/" alt="Disegno di una persona alla scrivania al computer di schiena con una camicia bianca e circondata da piante"> 
-</div> 
+         <div class="section-5">
+            <div class="section-5__left">
+               <img src="../assets/img/girl-computer-2.svg" alt="Disegno di una persona alla scrivania al computer di schiena con una camicia bianca e circondata da piante"> 
+            </div>  
+            <div class="section-5__right">
+               <p>
+                  Ritrova le tue
+                  <br>passioni
+               </p>
+               <p>
+                  Prenditi il tempo per te e 
+                  <br>per i tuoi interessi,
+                  <br><br>ora è FACILE.
+               </p>
+            </div>
+         </div>
 
-<?php //include 'footer.php';?>
-</body>
+         <div class="section-6">  
+            <p>
+               Per tutte
+               <br>le età
+            </p>
+            <p>
+               <br><br>Non esiste età GIUSTA
+               <br>Per imparare!
+            </p>
+            <div>
+               <img src="../assets/img/girl.jpg" alt=""> 
+               <img class="floating" src="../assets/img/family.png" alt="Disegno di 5 persone di età diverse che si abbracciano, impoteticamente una famiglia con nonni e bambini"> 
+            </div>
+         </div>
+         
+         <div class="section-7">
+            <div class="section-7__left">
+               <p>
+                  Impara
+                  <br>e divertiti
+               </p>
+               <p>
+                  Condividi le lezioni con le
+                  <br>persone che ami
+                  <br><br>Una nuova opportunità per
+                  <br>imparare e divertirsi INSIEME.
+               </p>
+            </div>
+            <div class="section-7__right">
+               <img src="../assets/img/people-table.svg" alt="Disegno di una persona alla scrivania al computer di schiena con una camicia bianca e circondata da piante"> 
+            </div>  
+         </div>
+         
+
+      </div>
+
+      <?php //include 'footer.php';?>
+   </body>
 </html>
