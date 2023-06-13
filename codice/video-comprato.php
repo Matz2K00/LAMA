c13f5ffddaafe17b88575f6f70ad13f0ec5209a0
Matz2K00 <matzeo2000@gmail.com>
2023-06-13 19:10:53 +0200

    stelline in riga

--- codice/video-comprato.php
+++ codice/video-comprato.php
@@ -56,11 +56,14 @@
 					for ($i = 0; $i < $gialla; $i++) { echo '<img src="../assets/icon/star/gialla.svg" alt="stella gialla" height="30">'; }
 					for ($i = 0; $i < $grigia; $i++) { echo '<img src="../assets/icon/star/grigia.svg" alt="stella grigia" height="30">'; }
 				}
-				
-				if ($valutazione === NULL) {
-					for ($i = 0; $i < 5; $i++) { echo '<img src="../assets/icon/star/grigia.svg" alt="stella grigia" height="30" class="star" data-value="'.$i.'">'; }
-				}
 			?>
+			<div class="stars">
+				<?php
+					if ($valutazione === NULL) {
+						for ($i = 0; $i < 5; $i++) { echo '<img src="../assets/icon/star/grigia.svg" alt="stella grigia" height="30" class="star" data-value="'.$i.'">'; }
+					}
+				?>
+			</div>
 		</div>
 		<?php if ($valutazione !== NULL){ echo '<p class="annullaValutazione">Annulla valutazione</p>';}?>
 	</div>
