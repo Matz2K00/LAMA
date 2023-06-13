f65014ce6e5543dd4061f2256675d26ac710dfcc
lallabelloni <belloni.laura00@gmail.com>
2023-06-13 21:07:22 +0200

    non lo so

--- codice/aggiornaValMedia.php
+++ codice/aggiornaValMedia.php
@@ -9,31 +9,27 @@ $result = $stmtCount->get_result();
 $row = $result->fetch_assoc();
 $nCorsi = intval($row['numero_righe']);
 
-// valutazione media
-$stmtVal = $connessione->prepare("UPDATE Corsi
-    SET valutazioneMedia = CAST(
-    (SELECT AVG(valutazione) AS media_valutazione
-    FROM Acquisti
-    WHERE id_corso = ? AND valutazione IS NOT NULL
-    )AS INT )WHERE id = ? ");
-
-// numero di valutazioni per corso
-$stmtNU = $connessione->prepare("UPDATE Corsi
-    SET nUtentiValut = (
-    SELECT COUNT(DISTINCT id_utente)
-    FROM Acquisti
-    WHERE id_corso = ? AND valutazione IS NOT NULL
-    )
-    WHERE id = ?");
 
 // aggiorno ogni corso
 for($num=1; $num<=$nCorsi; $num++){
-    
-    $stmtVal->bind_param("ii", $num, $num);
+// valutazione media
+    $stmtVal = $connessione->prepare("UPDATE Corsi
+        SET valutazioneMedia = CAST(
+        (SELECT AVG(valutazione) AS media_valutazione
+        FROM Acquisti
+        WHERE id_corso = $num AND valutazione IS NOT NULL
+        )AS INT )WHERE id = $num ");
     $stmtVal->execute();
 
-    $stmtNU->bind_param("ii", $num, $num);
-    $stmtNU->execute();
+    // numero di valutazioni per corso
+    $stmtNU = $connessione->prepare("UPDATE Corsi
+        SET nUtentiValut = (
+        SELECT COUNT(DISTINCT id_utente)
+        FROM Acquisti
+        WHERE id_corso = $num AND valutazione IS NOT NULL
+        )
+        WHERE id = $num");
+    $stmtNU->execute();    
 }
 $stmtCount->close();
 $stmtVal->close();
