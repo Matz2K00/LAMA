a1dd3b6773b2ab596855ac6789807411271a88e4
lallabelloni <belloni.laura00@gmail.com>
2023-06-13 02:00:55 +0200

    video NO comprato css

--- codice/video-NOcomprato.php
+++ codice/video-NOcomprato.php
@@ -35,122 +35,128 @@ if ($result->num_rows > 0) {
 $stmt->close();
 $conn->close();
 ?>  
-    <div>
-      <img src='<?php echo $urlImg; ?>' alt='<?php echo $altImg; ?>' height="300">
-      <h2><?php echo $titolo; ?></h2>
-      <p><?php echo $autore; ?></p>
-      <p><?php echo $descrizione; ?></p>
+
+<div class="video-wrapper">
+  <video width="50%" height="50%" controls>
+    <source src="../assets/video.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+  </video>
+</div>
+<div class="video-info">
+  <div class="video-info__left">
+    <!-- <img src='<?php echo $urlImg; ?>' alt='<?php echo $altImg; ?>' height="300"> -->
+    <h2><?php echo $titolo; ?></h2>
+    <p><?php echo $autore; ?></p>
+    <p><?php echo $descrizione; ?></p>
+  </div>
+  <div class="video-info__right">
+    <div class="valutazione">
+      <p>E' stato valutato</p>
+      <div> 
+          <?php //stelline
+          if ($valutazioneMedia !== NULL && $valutazioneMedia >= 0 && $valutazioneMedia < 6) {
+            $gialla = $valutazioneMedia;
+            $grigia = 5-$valutazioneMedia;
+            // togliere height="30"
+            for ($i = 0; $i < $gialla; $i++) { echo '<img src="../assets/icon/star/gialla.svg" alt="stella gialla" height="30">'; }
+            for ($i = 0; $i < $grigia; $i++) { echo '<img src="../assets/icon/star/grigia.svg" alt="stella grigia" height="30">'; }
+          }
+          if ($valutazioneMedia === NULL) {
+            for ($i = 0; $i < 5; $i++) { echo '<img src="../assets/icon/star/grigia.svg" alt="stella grigia" height="30">'; }
+          }
+          ?>
+      </div>
     </div>
-    
-<div class="valutazione">
-<p>E' stato valutato</p>
-<p> 
-    <?php //stelline
-    if ($valutazioneMedia !== NULL && $valutazioneMedia >= 0 && $valutazioneMedia < 6) {
-      $gialla = $valutazioneMedia;
-      $grigia = 5-$valutazioneMedia;
-      // togliere height="30"
-      for ($i = 0; $i < $gialla; $i++) { echo '<img src="../assets/icon/star/gialla.svg" alt="stella gialla" height="30">'; }
-      for ($i = 0; $i < $grigia; $i++) { echo '<img src="../assets/icon/star/grigia.svg" alt="stella grigia" height="30">'; }
-    }
-    if ($valutazioneMedia === NULL) {
-      for ($i = 0; $i < 5; $i++) { echo '<img src="../assets/icon/star/grigia.svg" alt="stella grigia" height="30">'; }
-    }
-    ?>
-</p>
-</div>
 
-<div class="prezzo">
-<p><?php echo $prezzo." €"; ?></p>
-<p>Disponibilità immediata</p>
-<p>Il corso sarà disponibile<br>in <b>I miei corsi</b><br>Non ha una scadenza</p>
-<p>Potrai guardarlo più volte<br>nell'ordine che preferisci</p>
+    <div class="prezzo">
+      <p><?php echo $prezzo." €"; ?></p>
+      <p>Disponibilità immediata</p>
+      <p>Il corso sarà disponibile<br>in <b>I miei corsi</b><br>Non ha una scadenza</p>
+      <p>Potrai guardarlo più volte<br>nell'ordine che preferisci</p>
 
-<?php 
-include 'cookie.php';
-if (in_array($id, $corsi)) {
-  
-  echo '<button>Aggiunto al carrello</button>';
-} else {
-  echo '<button id="aggiungiAlCarrello" onclick="showCheck()">Aggiungi al carrello</button>';
-}
-?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script>
-$(document).ready(function() {
-    $("#aggiungiAlCarrello").click(function() {
-      var id_corso = parseInt(<?php echo intval($id_corso);?>);
-        $.ajax({
-            url: "aggiungiAlCarrello.php", 
-            type: "POST",
-            data: { id_corso: id_corso }, 
-            success: function(response) {
-              setTimeout(function() {
-                location.reload();
-              }, 60000); // Ritardo di 1 minuto
-            }
-        });
-    });
-});
-function showCheck() {
-  var button = document.getElementById("aggiungiAlCarrello");
-  button.innerHTML = '<i class="fas fa-check"></i>';
-}
-</script>
-
-
-
-
-<?php // attenzione solo se schiacci il pulsante
-$_SESSION['id_corso'] = $id_corso;
-if(!isset($_SESSION['id_utente'])){
-  // $nome = 'acquistaOra';
-  // $acquisti = true;
-  // $scadenza = time() + 60;
-  // setcookie($nome, $acquisti, time() + 60, '/'); // un minuto
-  echo '<button class="primaAccedi">Acquista ora</button> ';
-?>
-  <script>
-  document.addEventListener('DOMContentLoaded', function() {
-    var button = document.querySelector('.primaAccedi');
-    var nome = 'acquistaOra';
-    var acquisti = true;
-    var scadenza = new Date();
-    scadenza.setTime(scadenza.getTime() + 30 * 1000);
-    var scadenzaString = scadenza.toUTCString();
-    button.addEventListener('click', function() {
-      document.cookie = nome + "=" + acquisti + "; expires=" + scadenzaString + "; path=/";
-      window.location.href = 'accesso.php';
-    });
-  });
-  </script>
-<?php
-} else {
-  echo '<button class="acquistaOra">Acquista ora</button> ';
-  ?>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="../typescript/click.js"></script>
-  <script>
-  $(document).ready(function() {
-    $(".acquistaOra").click(function() {
-      $.ajax({
-        url: "acquistaOra.php", 
-        type: "POST",
-        success: function(response) {
-          $("#rispostaAcquistaOra").html(response);
-        }
+      <?php 
+      include 'cookie.php';
+      if (in_array($id, $corsi)) {
+        
+        echo '<button class="carrelloBtn">Aggiunto al carrello</button>';
+      } else {
+        echo '<button class="carrelloBtn" id="aggiungiAlCarrello" onclick="showCheck()">Aggiungi al carrello</button>';
+      }
+      ?>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+      <script>
+      $(document).ready(function() {
+          $("#aggiungiAlCarrello").click(function() {
+            var id_corso = parseInt(<?php echo intval($id_corso);?>);
+              $.ajax({
+                  url: "aggiungiAlCarrello.php", 
+                  type: "POST",
+                  data: { id_corso: id_corso }, 
+                  success: function(response) {
+                    setTimeout(function() {
+                      location.reload();
+                    }, 60000); // Ritardo di 1 minuto
+                  }
+              });
+          });
       });
-    });
-  });
-  </script>
-  <div id="rispostaAcquistaOra"></div>
-  <?php
-}
-?>
+      function showCheck() {
+        var button = document.getElementById("aggiungiAlCarrello");
+        button.innerHTML = '<i class="fas fa-check"></i>';
+      }
+      </script>
 
-
+      <?php // attenzione solo se schiacci il pulsante
+        $_SESSION['id_corso'] = $id_corso;
+        if(!isset($_SESSION['id_utente'])){
+        // $nome = 'acquistaOra';
+        // $acquisti = true;
+        // $scadenza = time() + 60;
+        // setcookie($nome, $acquisti, time() + 60, '/'); // un minuto
+        echo '<button class="primaAccedi acquistaBtn">Acquista ora</button> ';
+      ?>
+        <script>
+          document.addEventListener('DOMContentLoaded', function() {
+            var button = document.querySelector('.primaAccedi');
+            var nome = 'acquistaOra';
+            var acquisti = true;
+            var scadenza = new Date();
+            scadenza.setTime(scadenza.getTime() + 30 * 1000);
+            var scadenzaString = scadenza.toUTCString();
+            button.addEventListener('click', function() {
+              document.cookie = nome + "=" + acquisti + "; expires=" + scadenzaString + "; path=/";
+              window.location.href = 'accesso.php';
+            });
+          });
+        </script>
+      <?php
+      } else {
+        echo '<button class="acquistaOra acquistaBtn">Acquista ora</button> ';
+        ?>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="../typescript/click.js"></script>
+        <script>
+        $(document).ready(function() {
+          $(".acquistaOra").click(function() {
+            $.ajax({
+              url: "acquistaOra.php", 
+              type: "POST",
+              success: function(response) {
+                $("#rispostaAcquistaOra").html(response);
+              }
+            });
+          });
+        });
+        </script>
+        <div id="rispostaAcquistaOra"></div>
+        <?php
+      }
+      ?>
+    </div>
+  </div>
 </div>
-  
+
+<div class="other" >
   <a href="cerca.php">Altri corsi</a>
 </div>
 
--- codice/video.php
+++ codice/video.php
@@ -3,10 +3,10 @@
 <head>
   <meta charset="UTF-8">
 
-  <!-- <link rel="stylesheet" type="text/css" href="../css/root.css"> -->
-  <!-- <link rel="stylesheet" type="text/css" href="../css/navbar.css"> -->
-  <!-- <link rel="stylesheet" type="text/css" href="../css/footer.css"> -->
-  <!-- <link rel="stylesheet" type="text/css" href=".../css/video.css"> -->
+  <link rel="stylesheet" type="text/css" href="../css/root.css">
+  <link rel="stylesheet" type="text/css" href="../css/navbar.css">
+  <link rel="stylesheet" type="text/css" href="../css/footer.css">
+  <link rel="stylesheet" type="text/css" href=".../css/video.css">
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
@@ -15,46 +15,47 @@
   <meta name="author " content="Belloni Laura, Contegno Matteo">
 </head>
 <body>
-<?php //include 'navbar.php';?>
+  <?php include 'navbar.php';?>
 
-<?php 
-require 'sessionStart.php';
-if(!isset($_SESSION['id_corso'])){
-  echo "<p> Non ci sono video selezionati</p>";
-  exit();
-}
-if(!isset($_SESSION['id_utente'])){
-  $id_corso = $_SESSION['id_corso'];
-  // echo "no sessione";
-  include 'video-NOcomprato.php';
-} else {
-  // echo "sessione";
-  $id_corso = $_SESSION['id_corso'];
-  $id_utente = $_SESSION['id_utente'];
+  <div class="video-page">
+    <?php 
+    require 'sessionStart.php';
+    if(!isset($_SESSION['id_corso'])){
+      echo "<p> Non ci sono video selezionati</p>";
+      exit();
+    }
+    if(!isset($_SESSION['id_utente'])){
+      $id_corso = $_SESSION['id_corso'];
+      // echo "no sessione";
+      include 'video-NOcomprato.php';
+    } else {
+      // echo "sessione";
+      $id_corso = $_SESSION['id_corso'];
+      $id_utente = $_SESSION['id_utente'];
 
-  require 'db.php';
-  $conn = new mysqli($hostData, $userData, $paswData, $database);
-  $sql = "SELECT * FROM Acquisti 
-          WHERE id_utente = ?
-          AND id_corso = ?";
-  $stmt = $conn->prepare($sql);
-  $stmt->bind_param("si", $id_utente, $id_corso);
-  $stmt->execute();
-  $result = $stmt->get_result();
+      require 'db.php';
+      $conn = new mysqli($hostData, $userData, $paswData, $database);
+      $sql = "SELECT * FROM Acquisti 
+              WHERE id_utente = ?
+              AND id_corso = ?";
+      $stmt = $conn->prepare($sql);
+      $stmt->bind_param("si", $id_utente, $id_corso);
+      $stmt->execute();
+      $result = $stmt->get_result();
 
-  if ($result->num_rows <= 0){
-    echo "<p>Non hai comprato questo corso </p>";
-    include 'video-NOcomprato.php';
-  }
-  else {
-    echo "<p>Hai comprato questo corso </p>";
-    include 'video-comprato.php';
-  }
-  $stmt->close();
-  $conn->close();
-}
-?>
-
-<?php //include 'footer.php';?>
+      if ($result->num_rows <= 0){
+        echo "<p>Non hai comprato questo corso </p>";
+        include 'video-NOcomprato.php';
+      }
+      else {
+        echo "<p>Hai comprato questo corso </p>";
+        include 'video-comprato.php';
+      }
+      $stmt->close();
+      $conn->close();
+    }
+    ?>
+  <div>
+  <?php include 'footer.php';?>
 </body>
 </html>
